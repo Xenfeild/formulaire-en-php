0bc2067264bb49c317c9cfd1b04e8804b964d8ef
Xenfeild <antonio.t350@gmail.com>
2023-03-14 11:08:20 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!-- header -->
+
+<?php 
+include('partials/_header.php');
+?>
+
+<!-- body -->
+<main class="pt-20">
+    <p class="text-xl">
+        Bienvenue en 2023. Dans ce cours nous allons apprendre le traitement des formulaires en php et les superglobals! 
+    </p>
+
+    </main>
+
+
+<!-- footer -->
+
+<?php 
+include('partials/_footer.php');
+
+?>
---
+++ partials/_header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- tailwind -->
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Formulaire en php</title>
+</head>
+<body class="p-20">
+    
+<header>
+
+</header>
+
+    <h1 class="font-blacl text-3xl text-center"> Cours N°2 cours en PHP</h1>
+    <nav class="text-center text-blue-500 pt-10 space-x-6">
+        <a href="index.php">Acceuil</a>
+        <a href="index.php">Get</a>
+        <a href="index.php">Post</a>
+        <a href="index.php">Validation formulaire</a>
+        <a href="index.php">Upload fichier</a>
+    </nav>
